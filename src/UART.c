29587695b30049e862b250d12a2c862edcd19008
慕芸婷 <12344332+xx3457388139@user.noreply.gitee.com>
2025-05-15 22:38:20 +0800

    优化串口初始化及中断处理

--- src/UART.c
+++ src/UART.c
@@ -7,7 +7,7 @@
  */
 void UART_Init()  //4800bps@11.0592MHz
 {
-    SCON=0x40; //设置串口工作模式1
+    SCON=0x50; //设置串口工作模式1
     PCON|= 0x80; //波特率加倍
     TMOD&=0x0F;//设置定时器1为工作方式2
     TMOD|=0x20;//设置定时器1为工作方式2
@@ -15,6 +15,8 @@ void UART_Init()  //4800bps@11.0592MHz
     TH1=0xf4; //设置定时器重载值
     TR1=1; //启动定时器1
     ET1=0; //禁止定时器1中断
+    EA=1; //使能总中断
+    ES=1; //使能串口中断
 }
 /**
  *  @name UART_SendByte
@@ -29,3 +31,16 @@ void UART_SendByte(unsigned char dat)
     while(!TI);
     TI=0;
 }
+
+//中断模板
+/*
+void UART_Routine() interrupt 4
+{
+    if(RI==1)
+    {
+        P2= SBUF; //将接收到的数据传送到P2口
+        UART_SendByte(SBUF); //将接收到的数据传回
+        RI=0; //清除接收中断标志位
+    }
+    
+}*/
--- src/main.c
+++ src/main.c
@@ -9,9 +9,30 @@ void Delay(unsigned int t)
         for(j=0;j<125;j++);
 }
 
+void UART_Init()  //4800bps@11.0592MHz
+{
+    SCON=0x50; //设置串口工作模式1
+    PCON|= 0x80; //波特率加倍
+    TMOD&=0x0F;//设置定时器1为工作方式2
+    TMOD|=0x20;//设置定时器1为工作方式2
+    TL1=0xf4; //设置定时器初值
+    TH1=0xf4; //设置定时器重载值
+    TR1=1; //启动定时器1
+    ET1=0; //禁止定时器1中断
+    EA=1; //使能总中断
+    ES=1; //使能串口中断
+}
 
-
-
+void UART_Routine() interrupt 4
+{
+    if(RI==1)
+    {
+        P2= SBUF; //将接收到的数据传送到P2口
+        UART_SendByte(SBUF); //将接收到的数据传回
+        RI=0; //清除接收中断标志位
+    }
+    
+}
 
 
 unsigned char sec=0;
@@ -21,9 +42,9 @@ void main()
      
      while(1)
      {
-         UART_SendByte(sec);
-         sec++;
-         Delay(1000);
+        //  UART_SendByte(sec);
+        //  sec++;
+        //  Delay(1000);
      }
   
 }
