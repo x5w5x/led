e54570b3ac0dbc028651324f6c84aa908e86368a
慕芸婷 <12344332+xx3457388139@user.noreply.gitee.com>
2025-05-16 23:34:47 +0800

    添加DS1302初始化及读写字节功能

--- src/DS1302.c
+++ src/DS1302.c
@@ -3,4 +3,47 @@
 sbit DS1302_SCLK = P3^6; //时钟
 sbit DS1302_IO = P3^4; //数据
 sbit DS1302_CE = P3^5;  //使能
-void  DS1302_WriteByte(unsigned char dat)
+
+
+void DS1302_Init()
+{
+    DS1302_CE=0;
+    DS1302_SCLK=0;
+
+}
+
+
+void  DS1302_WriteByte(unsigned char Command, unsigned char Data)
+{
+    unsigned char i;
+    DS1302_CE=1; //使能
+    for( i=0;i<8;i++)
+    {
+        DS1302_IO=Command&(0x01<<1); //写数据
+        DS1302_SCLK=1; 
+        
+        DS1302_SCLK=0;
+
+    }
+    for( i=0;i<8;i++)
+    {
+        DS1302_IO=Data&(0x01<<1); //写数据
+        DS1302_SCLK=1; 
+        
+        DS1302_SCLK=0;
+
+    }
+}
+
+unsigned char DS1302_ReadByte(unsigned char Command)
+{
+    unsigned char i,Data=0;
+    DS1302_CE=1; 
+    for( i=0;i<8;i++)
+    {
+        DS1302_IO=Command&(0x01<<1); //写数据
+        DS1302_SCLK=1; 
+        
+        DS1302_SCLK=0;
+    }
+}
