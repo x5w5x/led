815528bc46b29862a5b7f4b24eb712027c46d63d
慕芸婷 <12344332+xx3457388139@user.noreply.gitee.com>
2025-05-16 22:20:17 +0800

    添加MatrixLED显示及动画功能

--- src/MatrixLED.c
+++ src/MatrixLED.c
@@ -1,11 +1,15 @@
 #include<REGX52.H>
-
+#include<string.h>
 
 sbit RCK=P3^5; //74HC595寄存器锁存
 sbit SCK=P3^6;  //74HC595移位寄存器锁存
 sbit SER=P3^4;   //74HC595串行数据输入
 
 #define MatrixLED_Port P0 //LED矩阵端口
+
+
+
+
 void Delay(unsigned int t)
 {
     unsigned int i,j;
@@ -37,8 +41,8 @@ void HC595_WriteByte(unsigned char dat)
         dat<<=1; //dat左移一位，准备发送下一个位
         SCK=1;
     }
-    RCK=1;
-    RCK=0;
+    RCK=1; //锁存数据
+    RCK=0; //释放锁存
     
 }
 
@@ -47,7 +51,7 @@ void HC595_WriteByte(unsigned char dat)
  * 
  * @description: 74HC595显示一列数据
  * @param {unsigned char} column 列号
- * @param {unsigned char} dat 数据
+ * @param {unsigned char} dat 数据 16进制
  * @param：高位在上，低位在下
  * 
  * 
@@ -59,4 +63,77 @@ void MatrixLED_ShowColumn(unsigned char column, unsigned char dat)
     Delay(1);
     MatrixLED_Port=0xFF; //消影
     
+}
+/**
+ * 
+ * @description: 74HC595显示一串数据
+ * @param {unsigned char} dat[] 数据数组 8个字节
+ * @note：高位在上，低位在下
+ */
+void MatrixLED_Show(unsigned char dat[])
+{
+    unsigned char i;
+    for(i=0;i<8;i++)
+    {
+        MatrixLED_ShowColumn(i,dat[i]);
+    }
+}
+
+
+
+//必要定义
+unsigned char i,Offset=0,count=0; //定义变量
+/**
+ * 
+ * @description: 74HC595显示一串数据，滚动显示
+ * @param {unsigned char} dat[] 数据数组 8个字节
+ * @param {unsigned char} length 数据长度
+ * @note：高位在上，低位在下
+ * 
+ */
+void MatrixLED_FlowShow(unsigned char dat[],unsigned char length)
+{
+        for(i=0;i<8;i++)
+        {
+           MatrixLED_ShowColumn(i,dat[i+Offset]);
+
+        }
+        count++;
+        if(count>10) //控制滚动速度
+        {
+           count=0;
+           Offset++; //流动显示
+         //   Offset+=8; //快速显示（逐桢）
+           if(Offset>length-8)
+           {
+              Offset=0;
+           }
+        }
+
+}
+/**
+ * 
+ * @description: 74HC595显示一串数据，逐帧显示
+ * @param {unsigned char} dat[] 数据数组 8个字节
+ * @param {unsigned char} length 数据长度
+ * @note：高位在上，低位在下
+ * 
+ */
+void MatrixLED_AnimateShow(unsigned char dat[],unsigned char length)
+{
+      for(i=0;i<8;i++)
+        {
+           MatrixLED_ShowColumn(i,dat[i+Offset]);
+
+        }
+        count++;
+        if(count>10) //控制滚动速度
+        {
+           count=0;
+           Offset+=8; //快速显示（逐桢）
+           if(Offset>length-8)
+           {
+              Offset=0;
+           }
+        }
 }
--- src/MatrixLED.h
+++ src/MatrixLED.h
@@ -2,6 +2,7 @@
 #define _MATRIXLED_H
 void MatrixLED_Init();
 void MatrixLED_ShowColumn(unsigned char column, unsigned char dat);
-
-
+void MatrixLED_Show(unsigned char dat[]);
+void MatrixLED_FlowShow(unsigned char dat[],unsigned char length);
+void MatrixLED_AnimateShow(unsigned char dat[],unsigned char length);
 #endif
