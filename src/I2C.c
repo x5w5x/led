eb4f6db29f5a835d4390dadc6199c2026aa8e019
慕芸婷 <12344332+xx3457388139@user.noreply.gitee.com>
2025-05-21 23:47:23 +0800

    完善

--- src/I2C.c
+++ src/I2C.c
@@ -10,7 +10,8 @@ sbit I2C_SCL = P2^0;
  * @param 无
  * @return 无
  */
-void I2C_Start(){
+void I2C_Start()
+{
     I2C_SDA = 1;
     I2C_SCL = 1;
     I2C_SDA = 0;
@@ -23,7 +24,8 @@ void I2C_Start(){
  * @param 无
  * @return 无
  */
-void I2C_Stop(){
+void I2C_Stop()
+{
     I2C_SDA = 0;
     I2C_SCL = 1;
     I2C_SDA = 1;
@@ -35,7 +37,8 @@ void I2C_Stop(){
  * @param dat:要发送的数据
  * @return 无
  */
-void I2C_SendByte(unsigned char dat){
+void I2C_SendByte(unsigned char dat)
+{
     unsigned char i;
     for(i=0;i<8;i++){
         I2C_SDA = dat & (0x80);
@@ -53,7 +56,8 @@ void I2C_SendByte(unsigned char dat){
  * @param 无
  * @return 接收到的数据
  */
-unsigned char I2C_ReceiveByte(){
+unsigned char I2C_ReceiveByte()
+{
     unsigned char i,dat = 0x00;
     I2C_SDA = 1;
     for(i=0;i<8;i++){
@@ -70,7 +74,8 @@ unsigned char I2C_ReceiveByte(){
  * @param Ack:ACK信号
  * @return 无
  */
-void I2C_SendACK(unsigned char Ack){
+void I2C_SendACK(unsigned char Ack)
+{
     I2C_SDA = ack;
     I2C_SCL = 1;
     I2C_SCL = 0;
@@ -82,7 +87,8 @@ void I2C_SendACK(unsigned char Ack){
  * @param 无
  * @return 接收到的ACK信号
  */
-unsigned char I2C_ReceiveACK(){
+unsigned char I2C_ReceiveACK()
+{
     unsigned char Ack;
     I2C_SDA = 1; //释放SDA
     I2C_SCL = 1;
--- src/I2C.h
+++ src/I2C.h
@@ -1,6 +1,11 @@
 #ifndef _I2C_H
 #define _I2C_H
 
-
+void I2C_Start();
+void I2C_Stop();
+void I2C_SendByte(unsigned char dat);
+unsigned char I2C_ReceiveByte();
+void I2C_SendACK(unsigned char Ack);
+unsigned char I2C_ReceiveACK();
 
 #endif
