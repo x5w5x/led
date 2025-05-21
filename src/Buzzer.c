1bf5b376e1bdce5aa5516fcc1f3cf12ac0a2b4fb
慕芸婷 <12344332+xx3457388139@user.noreply.gitee.com>
2025-05-21 13:09:23 +0800

    优化蜂鸣器延时函数及注释

--- src/Buzzer.c
+++ src/Buzzer.c
@@ -1,7 +1,7 @@
 #include<REGX52.H>
 
 sbit Buzzer = P2^5; //蜂鸣器连接口
-void Delay500us(void)	//@11.0592MHz
+void BuzzerDelay500us(void)	//@11.0592MHz
 {
 	unsigned char data i;
 
@@ -9,13 +9,19 @@ void Delay500us(void)	//@11.0592MHz
 	i = 227;
 	while (--i);
 }
-
+/**
+ * @name: Buzzer_Time
+ * @brief: 蜂鸣器延时函数
+ * @param {unsigned int} ms
+ * @return {*}
+ * 
+ */
 void Buzzer_Time(unsigned int ms)
 {   unsigned char i;
    for( i=0;i<ms;i++)
          {
           Buzzer=!Buzzer;
-          Delay500us();
+          BuzzerDelay500us();
 
       }
 }
